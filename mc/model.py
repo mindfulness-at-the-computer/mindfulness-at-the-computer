71d46a1e8679ce27c9ae75b51e0a9d7e81cb794b
Yigit Sever <yigit.sever@tedu.edu.tr>
2017-12-26 01:32:04 +0300

    Added type hints to function headers at mc/model.py #71

--- mc/model.py
+++ mc/model.py
@@ -145,17 +145,17 @@ class PhrasesM:
         return ret_reminder_list
 
     @staticmethod
-    def remove(i_id_int):
+    def remove(i_id: int):
         db_connection = db.Helper.get_db_connection()
         db_cursor = db_connection.cursor()
         db_cursor.execute(
             "DELETE FROM " + db.Schema.PhrasesTable.name
             + " WHERE " + db.Schema.PhrasesTable.Cols.id + "=?",
-            (str(i_id_int),)
+            (str(i_id),)
         )
         db_connection.commit()
 
-    def _update(self, i_col_name: str, i_new_value):
+    def _update(self, i_col_name: str, i_new_value: int):
         db_exec(
             "UPDATE " + db.Schema.PhrasesTable.name
             + " SET " + i_col_name + " = ?"
@@ -338,13 +338,13 @@ class RestActionsM:
         # -the asterisk (*) will "expand" the tuple into separate arguments for the function header
 
     @staticmethod
-    def remove(i_id_int):
+    def remove(i_id: int):
         db_connection = db.Helper.get_db_connection()
         db_cursor = db_connection.cursor()
         db_cursor.execute(
             "DELETE FROM " + db.Schema.RestActionsTable.name
             + " WHERE " + db.Schema.RestActionsTable.Cols.id + "=?",
-            (str(i_id_int), )
+            (str(i_id), )
         )
         db_connection.commit()
 
