1c6eeb2d5060b1470ed5d377875965c1c627e497
☮Tord☃ <tord.dellsen@gmail.com>
2018-01-12 18:42:10 +0100

    New translations source_en.ts (Chinese Simplified)

---
+++ translate/zh_CN/zh_CN.ts
@@ -0,0 +1,429 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh-CN" sourcelanguage="en">
+  <context>
+    <name>BreathingDlg</name>
+    <message>
+      <location filename="mc/gui/breathing_dlg.py" line="59"/>
+      <source>Close</source>
+      <translation type="unfinished">Close</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_dlg.py" line="64"/>
+      <source>Hover over the central area breathing in and over the background breathing out</source>
+      <translation type="unfinished">Hover over the central area breathing in and over the background breathing out</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_dlg.py" line="73"/>
+      <source>Use shortened</source>
+      <translation type="unfinished">Use shortened</translation>
+    </message>
+  </context>
+  <context>
+    <name>BreathingNotification</name>
+    <message>
+      <location filename="mc/gui/breathing_notification.py" line="52"/>
+      <source>Open Dialog</source>
+      <translation type="unfinished">Open Dialog</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_notification.py" line="58"/>
+      <source>Close</source>
+      <translation type="unfinished">Close</translation>
+    </message>
+  </context>
+  <context>
+    <name>BreathingPhraseListWt</name>
+    <message>
+      <location filename="mc/gui/breathing_phrase_list_wt.py" line="34"/>
+      <source>New item</source>
+      <translation type="unfinished">New item</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_phrase_list_wt.py" line="42"/>
+      <source>Add</source>
+      <translation type="unfinished">Add</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_phrase_list_wt.py" line="51"/>
+      <source>Edit the selected breathing phrase</source>
+      <translation type="unfinished">Edit the selected breathing phrase</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_phrase_list_wt.py" line="57"/>
+      <source>Move the selected breathing phrase to top</source>
+      <translation type="unfinished">Move the selected breathing phrase to top</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_phrase_list_wt.py" line="63"/>
+      <source>Move the selected breathing phrase up</source>
+      <translation type="unfinished">Move the selected breathing phrase up</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_phrase_list_wt.py" line="69"/>
+      <source>Move the selected breathing phrase down</source>
+      <translation type="unfinished">Move the selected breathing phrase down</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_phrase_list_wt.py" line="77"/>
+      <source>Delete the selected breathing phrase</source>
+      <translation type="unfinished">Delete the selected breathing phrase</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_phrase_list_wt.py" line="142"/>
+      <source>Are you sure that you want to remove this entry?</source>
+      <translation type="unfinished">Are you sure that you want to remove this entry?</translation>
+    </message>
+  </context>
+  <context>
+    <name>BreathingSettingsWt</name>
+    <message>
+      <location filename="mc/gui/breathing_settings_wt.py" line="33"/>
+      <source>Notifications</source>
+      <translation type="unfinished">Notifications</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_settings_wt.py" line="40"/>
+      <source>Notification type: </source>
+      <translation type="unfinished">Notification type: </translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_settings_wt.py" line="53"/>
+      <source>Interval:</source>
+      <translation type="unfinished">Interval:</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_settings_wt.py" line="60"/>
+      <source>minutes</source>
+      <translation type="unfinished">minutes</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_settings_wt.py" line="64"/>
+      <source>Dialog</source>
+      <translation type="unfinished">Dialog</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_settings_wt.py" line="71"/>
+      <source>Phrase setup: </source>
+      <translation type="unfinished">Phrase setup: </translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_settings_wt.py" line="84"/>
+      <source>Show after</source>
+      <translation type="unfinished">Show after</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_settings_wt.py" line="88"/>
+      <source>notifications</source>
+      <translation type="unfinished">notifications</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_settings_wt.py" line="91"/>
+      <source>Play Audio</source>
+      <translation type="unfinished">Play Audio</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_settings_wt.py" line="95"/>
+      <source>Open breathing dialog</source>
+      <translation type="unfinished">Open breathing dialog</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_settings_wt.py" line="100"/>
+      <source>Audio</source>
+      <translation type="unfinished">Audio</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_settings_wt.py" line="104"/>
+      <source>Select audio</source>
+      <translation type="unfinished">Select audio</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_settings_wt.py" line="149"/>
+      <source>Please choose a wav audio file</source>
+      <translation type="unfinished">Please choose a wav audio file</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_settings_wt.py" line="149"/>
+      <source>Wav files (*.wav)</source>
+      <translation type="unfinished">Wav files (*.wav)</translation>
+    </message>
+  </context>
+  <context>
+    <name>EditDialog</name>
+    <message>
+      <location filename="mc/gui/rest_action_list_wt.py" line="191"/>
+      <source>Title</source>
+      <translation type="unfinished">Title</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_phrase_list_wt.py" line="264"/>
+      <source>In breath phrase</source>
+      <translation type="unfinished">In breath phrase</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_phrase_list_wt.py" line="268"/>
+      <source>Out breath phrase</source>
+      <translation type="unfinished">Out breath phrase</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_phrase_list_wt.py" line="272"/>
+      <source>Short in breath phrase</source>
+      <translation type="unfinished">Short in breath phrase</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/breathing_phrase_list_wt.py" line="276"/>
+      <source>Short out breath phrase</source>
+      <translation type="unfinished">Short out breath phrase</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_action_list_wt.py" line="199"/>
+      <source>Image</source>
+      <translation type="unfinished">Image</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_action_list_wt.py" line="203"/>
+      <source> Select image</source>
+      <translation type="unfinished"> Select image</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_action_list_wt.py" line="237"/>
+      <source>image does not exist</source>
+      <translation type="unfinished">image does not exist</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_action_list_wt.py" line="239"/>
+      <source>(no image set)</source>
+      <translation type="unfinished">(no image set)</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_action_list_wt.py" line="267"/>
+      <source>Please choose an image</source>
+      <translation type="unfinished">Please choose an image</translation>
+    </message>
+  </context>
+  <context>
+    <name>MainWin</name>
+    <message>
+      <location filename="mc/gui/main_win.py" line="85"/>
+      <source>title</source>
+      <translation type="unfinished">title</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/main_win.py" line="88"/>
+      <source>in</source>
+      <translation type="unfinished">in</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/main_win.py" line="91"/>
+      <source>out</source>
+      <translation type="unfinished">out</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/main_win.py" line="177"/>
+      <source>Enable Rest Reminder</source>
+      <translation type="unfinished">Enable Rest Reminder</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/main_win.py" line="188"/>
+      <source>Take a Break Now</source>
+      <translation type="unfinished">Take a Break Now</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/main_win.py" line="194"/>
+      <source>Enable Breathing Reminder</source>
+      <translation type="unfinished">Enable Breathing Reminder</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/main_win.py" line="202"/>
+      <source>Open Breathing Dialog</source>
+      <translation type="unfinished">Open Breathing Dialog</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/main_win.py" line="234"/>
+      <source>Open Settings</source>
+      <translation type="unfinished">Open Settings</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/main_win.py" line="389"/>
+      <source>Quit</source>
+      <translation type="unfinished">Quit</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/main_win.py" line="382"/>
+      <source>&amp;File</source>
+      <translation type="unfinished">&amp;File</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/main_win.py" line="383"/>
+      <source>Export data</source>
+      <translation type="unfinished">Export data</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/main_win.py" line="386"/>
+      <source>Minimize to tray</source>
+      <translation type="unfinished">Minimize to tray</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/main_win.py" line="397"/>
+      <source>Full screen</source>
+      <translation type="unfinished">Full screen</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/main_win.py" line="400"/>
+      <source>Show rest reminder</source>
+      <translation type="unfinished">Show rest reminder</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/main_win.py" line="407"/>
+      <source>&amp;Help</source>
+      <translation type="unfinished">&amp;Help</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/main_win.py" line="408"/>
+      <source>About</source>
+      <translation type="unfinished">About</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/main_win.py" line="414"/>
+      <source>System Information</source>
+      <translation type="unfinished">System Information</translation>
+    </message>
+  </context>
+  <context>
+    <name>RestActionListWt</name>
+    <message>
+      <location filename="mc/gui/rest_action_list_wt.py" line="31"/>
+      <source>Add</source>
+      <translation type="unfinished">Add</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_action_list_wt.py" line="42"/>
+      <source>Edit the selected rest action</source>
+      <translation type="unfinished">Edit the selected rest action</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_action_list_wt.py" line="48"/>
+      <source>Move the selected rest action to top</source>
+      <translation type="unfinished">Move the selected rest action to top</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_action_list_wt.py" line="54"/>
+      <source>Move the selected rest action up</source>
+      <translation type="unfinished">Move the selected rest action up</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_action_list_wt.py" line="60"/>
+      <source>Move the selected rest action down</source>
+      <translation type="unfinished">Move the selected rest action down</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_action_list_wt.py" line="68"/>
+      <source>Delete the selected rest action</source>
+      <translation type="unfinished">Delete the selected rest action</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_action_list_wt.py" line="125"/>
+      <source>Are you sure that you want to remove this entry?</source>
+      <translation type="unfinished">Are you sure that you want to remove this entry?</translation>
+    </message>
+  </context>
+  <context>
+    <name>RestReminderDlg</name>
+    <message>
+      <location filename="mc/gui/rest_notification.py" line="34"/>
+      <source>Please take good care of your body and mind</source>
+      <translation type="unfinished">Please take good care of your body and mind</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_notification.py" line="40"/>
+      <source>Rest</source>
+      <translation type="unfinished">Rest</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_notification.py" line="47"/>
+      <source>Wait</source>
+      <translation type="unfinished">Wait</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_notification.py" line="51"/>
+      <source>Skip</source>
+      <translation type="unfinished">Skip</translation>
+    </message>
+  </context>
+  <context>
+    <name>RestSettingsWt</name>
+    <message>
+      <location filename="mc/gui/rest_settings_wt.py" line="34"/>
+      <source>Interval:</source>
+      <translation type="unfinished">Interval:</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_settings_wt.py" line="37"/>
+      <source>minutes</source>
+      <translation type="unfinished">minutes</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_settings_wt.py" line="42"/>
+      <source>Time until next break:</source>
+      <translation type="unfinished">Time until next break:</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_settings_wt.py" line="54"/>
+      <source>Reset the rest timer</source>
+      <translation type="unfinished">Reset the rest timer</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_settings_wt.py" line="77"/>
+      <source>Take a break now</source>
+      <translation type="unfinished">Take a break now</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_settings_wt.py" line="84"/>
+      <source>Notification type: </source>
+      <translation type="unfinished">Notification type: </translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_settings_wt.py" line="96"/>
+      <source>Audio</source>
+      <translation type="unfinished">Audio</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_settings_wt.py" line="100"/>
+      <source>Select audio</source>
+      <translation type="unfinished">Select audio</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_settings_wt.py" line="131"/>
+      <source>Please choose a wav audio file</source>
+      <translation type="unfinished">Please choose a wav audio file</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/rest_settings_wt.py" line="131"/>
+      <source>Wav files (*.wav)</source>
+      <translation type="unfinished">Wav files (*.wav)</translation>
+    </message>
+  </context>
+  <context>
+    <name>ToggleSwitchWt</name>
+    <message>
+      <location filename="mc/gui/toggle_switch_wt.py" line="22"/>
+      <source>On</source>
+      <translation type="unfinished">On</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/toggle_switch_wt.py" line="28"/>
+      <source>Off</source>
+      <translation type="unfinished">Off</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/toggle_switch_wt.py" line="61"/>
+      <source>Enabled</source>
+      <translation type="unfinished">Enabled</translation>
+    </message>
+    <message>
+      <location filename="mc/gui/toggle_switch_wt.py" line="66"/>
+      <source>Disabled</source>
+      <translation type="unfinished">Disabled</translation>
+    </message>
+  </context>
+</TS>
