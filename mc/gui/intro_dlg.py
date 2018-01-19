e4c7ff852068e79ec6adc19c10db7b2edb4cb1c5
Tord <tord.dellsen@gmail.com>
2018-01-19 02:07:39 +0100

    More text added to the first wizard page

--- mc/gui/intro_dlg.py
+++ mc/gui/intro_dlg.py
@@ -1,14 +1,12 @@
-import os
 import logging
 import mc.model
 from PyQt5 import QtCore
-from PyQt5 import QtGui
 from PyQt5 import QtWidgets
-
-from mc import model, mc_global
 import mc.gui.breathing_dlg
+import mc.mc_global
 
 NEXT_STR = "Next >>"
+PARAGRAPH_SPACING_INT = 10
 
 
 class IntroDlg(QtWidgets.QDialog):
@@ -35,11 +33,6 @@ class IntroDlg(QtWidgets.QDialog):
         hbox_l3.addWidget(self.next_qpb, stretch=1)
         hbox_l3.addStretch(1)
         self.next_qpb.clicked.connect(self.on_next_clicked)
-        """
-        self.close_qpb = QtWidgets.QPushButton("Close")
-        hbox_l3.addWidget(self.close_qpb)
-        self.close_qpb.clicked.connect(self.on_close_clicked)
-        """
 
         self.info = InformationPage()
         self.wizard_qsw_w3.addWidget(self.info)
@@ -50,29 +43,9 @@ class IntroDlg(QtWidgets.QDialog):
         self.breathing_dialog_coming = BreathingDialogComing()
         self.wizard_qsw_w3.addWidget(self.breathing_dialog_coming)
 
-        """
-        self.wizard_qll = QtWidgets.QLabel(self.tr("3. Please choose your initial settings"))
-        vbox_l2.addWidget(self.wizard_qll)
-        self.setting_example_interval_qsb = QtWidgets.QSpinBox()
-        vbox_l2.addWidget(self.setting_example_interval_qsb)
-        self.setting_example_audio_qcb = QtWidgets.QCheckBox("Audio active")
-        vbox_l2.addWidget(self.setting_example_audio_qcb)
-
-        vbox_l2.addSpacing(40)
-
-        self.close_qpb = QtWidgets.QPushButton("Close")
-        vbox_l2.addWidget(self.close_qpb)
-        self.close_qpb.clicked.connect(self.on_close_clicked)
-        """
-
         self.update_gui()
 
-        # self.setup_rest_action_list()
         self.show()
-
-    def on_close_clicked(self):
-        self.close_signal.emit(False)
-        self.close()
 
     def on_next_clicked(self):
         current_index_int = self.wizard_qsw_w3.currentIndex()
@@ -108,16 +81,30 @@ class InformationPage(QtWidgets.QWidget):
         self.setLayout(vbox_l2)
 
         self.title_qll = QtWidgets.QLabel("Introduction")
-        self.title_qll.setFont(mc.mc_global.get_font_large())
+        self.title_qll.setFont(mc.mc_global.get_font_xlarge())
         vbox_l2.addWidget(self.title_qll)
 
         vbox_l2.addStretch(1)
 
-        self.text_qll = QtWidgets.QLabel('''
-Welcome! Please take a few moments to set read about and up the application. Or you can skip and return to this wizard later by going to "help" -> "setup wizard"
-''')
+        self.text_qll = QtWidgets.QLabel(
+            'Welcome! You can use this wizard to read about and set up the application. '
+            'You can return to this wizard later by going to "help" -> "setup wizard"'
+        )
         self.text_qll.setWordWrap(True)
         vbox_l2.addWidget(self.text_qll)
+
+        vbox_l2.addSpacing(PARAGRAPH_SPACING_INT)
+
+        self.text_2_qll = QtWidgets.QLabel(
+            "This application reminds you to stay aware of your breathing and to take breaks. "
+            "These are the parts of the interface:"
+            "\n1. The breathing dialog and notifications"
+            "\n2. The rest dialog and notifications"
+            "\n3. The settings window"
+            "\n4. The system tray icon and menu"
+        )
+        self.text_2_qll.setWordWrap(True)
+        vbox_l2.addWidget(self.text_2_qll)
 
         vbox_l2.addStretch(1)
 
@@ -129,14 +116,16 @@ class BreathingDialogComing(QtWidgets.QWidget):
         self.setLayout(vbox_l2)
 
         self.title_qll = QtWidgets.QLabel("Breathing Dialog")
-        self.title_qll.setFont(mc.mc_global.get_font_large())
+        self.title_qll.setFont(mc.mc_global.get_font_xlarge())
         vbox_l2.addWidget(self.title_qll)
 
         vbox_l2.addStretch(1)
 
-        self.text_qll = QtWidgets.QLabel('''
-When you click on finish a breathing dialog will be shown. You can use it by ______ or using the keyboard
-''')
+        self.text_qll = QtWidgets.QLabel(
+            "When you click on finish and exit this wizard a breathing dialog will be shown. "
+            "You can use it by holding the mouse cursor over the central breathing square while breathing in, "
+            "and over the background area/rectangle while breathing out"
+        )
         self.text_qll.setWordWrap(True)
         vbox_l2.addWidget(self.text_qll)
 
@@ -146,16 +135,22 @@ When you click on finish a breathing dialog will be shown. You can use it by ___
 class BreathingInitSetupPage(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+
+        settings = mc.model.SettingsM.get()
+
         vbox_l2 = QtWidgets.QVBoxLayout()
         self.setLayout(vbox_l2)
 
         self.title_qll = QtWidgets.QLabel("Initial setup (breathing)")
-        self.title_qll.setFont(mc.mc_global.get_font_large())
+        self.title_qll.setFont(mc.mc_global.get_font_xlarge())
         vbox_l2.addWidget(self.title_qll)
 
         vbox_l2.addStretch(1)
 
-        self.text_qll = QtWidgets.QLabel("Please select the intial setup parameters for the breathing. (Or use the default values)")
+        self.text_qll = QtWidgets.QLabel(
+            "Please select the initial setup parameters for the breathing dialog. "
+            "It's possible to change the values later on using the settings window. "
+        )
         self.text_qll.setWordWrap(True)
         vbox_l2.addWidget(self.text_qll)
 
@@ -166,7 +161,10 @@ class BreathingInitSetupPage(QtWidgets.QWidget):
         hbox_l3.addWidget(QtWidgets.QLabel("Time between breathing notifications"))
         self.time_btw_notifications_qsb = QtWidgets.QSpinBox()
         hbox_l3.addWidget(self.time_btw_notifications_qsb)
-        self.time_btw_notifications_qsb.valueChanged.connect(self.on_time_btw_notifications_valuechanged)
+        self.time_btw_notifications_qsb.valueChanged.connect(
+            self.on_time_btw_notifications_value_changed
+        )
+        self.time_btw_notifications_qsb.setValue(settings.breathing_reminder_interval_int)
 
         vbox_l2.addStretch(1)
 
@@ -176,8 +174,8 @@ class BreathingInitSetupPage(QtWidgets.QWidget):
         self.play_audio_qcb.toggled.connect(self.on_play_audio_toggled)
         """
 
-    def on_time_btw_notifications_valuechanged(self, i_new_value: int):
-        logging.debug("on_time_btw_notifications_valuechanged, i_new_value = " + str(i_new_value))
+    def on_time_btw_notifications_value_changed(self, i_new_value: int):
+        logging.debug("on_time_btw_notifications_value_changed, i_new_value = " + str(i_new_value))
         mc.model.SettingsM.update_breathing_reminder_interval(i_new_value)
 
     def on_play_audio_toggled(self, i_checked: bool):
