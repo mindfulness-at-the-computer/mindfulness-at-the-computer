a0121cf065cdf9e7a08809ccb786a2123f7d5665
J-nix <jlhorner@protonmail.com>
2018-02-05 13:38:49 +0000

    Type hint formatting

--- mc/db.py
+++ mc/db.py
@@ -17,16 +17,16 @@ MAX_VOLUME_INT = 100
 DEFAULT_BREATHING_REMINDER_NR_BEFORE_DIALOG_INT = 3
 
 
-def get_schema_version(i_db_conn :sqlite3.Connection) -> int:
+def get_schema_version(i_db_conn: sqlite3.Connection) -> int:
     t_cursor = i_db_conn.execute("PRAGMA user_version")
     return t_cursor.fetchone()[0]
 
 
-def set_schema_version(i_db_conn, i_version_it :sqlite3.Connection) -> None:
+def set_schema_version(i_db_conn, i_version_it: sqlite3.Connection) -> None:
     i_db_conn.execute("PRAGMA user_version={:d}".format(i_version_it))
 
 
-def initial_schema_and_setup(i_db_conn :sqlite3.Connection) -> None:
+def initial_schema_and_setup(i_db_conn: sqlite3.Connection) -> None:
     # Auto-increment is not needed in our case: https://www.sqlite.org/autoinc.html
 
     i_db_conn.execute(
@@ -101,7 +101,7 @@ def upgrade_1_2(i_db_conn):
 """
 
 
-def upgrade_1_2(i_db_conn :sqlite3.Connection) -> None:
+def upgrade_1_2(i_db_conn: sqlite3.Connection) -> None:
     backup_db_file()
     i_db_conn.execute(
         "ALTER TABLE " + Schema.SettingsTable.name + " ADD COLUMN "
@@ -110,7 +110,7 @@ def upgrade_1_2(i_db_conn :sqlite3.Connection) -> None:
     )
 
 
-def upgrade_2_3(i_db_conn :sqlite3.Connection) -> None:
+def upgrade_2_3(i_db_conn: sqlite3.Connection) -> None:
     backup_db_file()
     i_db_conn.execute(
         "ALTER TABLE " + Schema.SettingsTable.name + " ADD COLUMN "
@@ -119,7 +119,7 @@ def upgrade_2_3(i_db_conn :sqlite3.Connection) -> None:
     )
 
 
-def upgrade_3_4(i_db_conn :sqlite3.Connection) -> None:
+def upgrade_3_4(i_db_conn: sqlite3.Connection) -> None:
     backup_db_file()
     i_db_conn.execute(
         "ALTER TABLE " + Schema.PhrasesTable.name + " ADD COLUMN "
@@ -183,7 +183,7 @@ class Helper(object):
         return Helper.__db_connection
 
     @staticmethod
-    def drop_all_db_tables(i_db_conn :sqlite3.Connection) -> None:
+    def drop_all_db_tables(i_db_conn: sqlite3.Connection) -> None:
         Helper.drop_db_table(i_db_conn, Schema.PhrasesTable.name)
         Helper.drop_db_table(i_db_conn, Schema.RestActionsTable.name)
         Helper.drop_db_table(i_db_conn, Schema.SettingsTable.name)
