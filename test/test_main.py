676de3c7ae3a189a08740cc81522b753fa0911ac
Theonimfi <theon.anog@gmail.com>
2018-05-07 23:00:08 +0300

    Add a test for choose music button.

--- test/test_main.py
+++ test/test_main.py
@@ -88,6 +88,13 @@ class MainTest(unittest.TestCase):
         QtTest.QTest.mouseClick(ok_dialog_button, QtCore.Qt.LeftButton)
         QtTest.QTest.waitForEvents()
 
+    def test_choose_music_button(self):
+        choose_music = mc.gui.rest_dlg.RestDlg()
+        if choose_music.choose_music_qpb.isEnabled():
+            self.assertTrue(choose_music.on_choose_music_clicked)
+        else:
+            self.assertFalse(choose_music.on_choose_music_clicked)
+
     """
     def test_starting_breathing(self):
         main_win_widget = mc.gui.main_win.MbMainWindow()
